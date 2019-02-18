319154cd0b4ed366007a456241cf83df053da6bf
Mark <mark.ioshpa@gmail.com>
2019-02-18 11:08:41 +0200

    Restaurant Menu Service

---
+++ consts.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MLAB_URL: "mongodb://ds261644.mlab.com:61644/restaurant_menu",
+    DB_USER: "MarkIoshpa",
+    DB_PASS: "gosu60"
+}
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
     res.set("Content-Type", "application/json");
     next();
