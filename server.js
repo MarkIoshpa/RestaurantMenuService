d47fff2080ca3001c73aa6715060c78c1cb53ff7
Mark <mark.ioshpa@gmail.com>
2019-02-18 11:47:37 +0200

    Restaurant Menu Service

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
     res.set("Content-Type", "application/json");
     next();
 })
